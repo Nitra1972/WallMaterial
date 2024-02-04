62e056b1104668b4c8308474a5b1eb6f7cee548e
NITRA <nitra@inbox.ru>
2024-02-04 09:46:18 +0300

    Ничего лишнего

--- GetAirDeffuser/WallOpperation/WallMaterialForm.cs
+++ GetAirDeffuser/WallOpperation/WallMaterialForm.cs
@@ -34,7 +34,6 @@ namespace GetAirDeffuser.DiffuserOpperation
             {
                 foreach (Element elType in new ElemensCollectionOfType().GetCollection(doc, bultInCat))
                 {
-
                     if (elType is WallType wallType)
                     {
                         try
@@ -45,7 +44,6 @@ namespace GetAirDeffuser.DiffuserOpperation
                         catch { }
                         
                     }
-
                     if (elType is FloorType floorType)
                     {
                         try
@@ -59,18 +57,18 @@ namespace GetAirDeffuser.DiffuserOpperation
                         try
                         {
                             roofType.Name = new LayerWidth().OperateLayer(roofType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0), doc, string.Empty);
-                    }
+                        }
                         catch { }
-                }
+                    }
                     if (elType is CeilingType ceilingType)
                     {
-                        //foreach (CompoundStructureLayer layer in ceilingType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0))
-                        //{
-                        //    CeilingType.Name = GetLayerForm(count, laireName) + "_" + OperateLayer(layer, doc, exemplaireName);
-                        //}
+                        try
+                        {
+                            ceilingType.Name = new LayerWidth().OperateLayer(ceilingType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0), doc, string.Empty);
+                        }
+                        catch { }
                     }
-
-
+                   
                 }
             }
         }
@@ -80,145 +78,6 @@ namespace GetAirDeffuser.DiffuserOpperation
 
                 
     
-
-
-
-
-
-
-
-//        if (elType is WallType wallType)
-//    {
-//        var exType = wallType as WallType;
-//    }
-//    if (elType is FloorType floorType)
-//    {
-//        var exType = floorType as FloorType;
-//    }
-//    if (elType is RoofType roofType)
-//    {
-//        var exType = roofType as RoofType;
-//    }
-//    if (elType is CeilingType ceilingType)
-//    {
-//        var exType = ceilingType as CeilingType;
-//    }
-
-//        foreach (CompoundStructureLayer layer in colletion)
-//    {
-//        wallType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0);
-//        wallType.Name = GetLayerForm(count, exemplaireName) + "_" + totalExemplaireWidth.ToString();
-//    }
-
-//    if (elType is WallType wallType)
-//        WallType.Name = GetLayerForm(count, laireName) + "_" + totalExemplaireWidth.ToString();
-//    return WallType.Name;
-//}
-
-
-
-
-
-
-//public void GetWallMaterial(Document doc)
-//{
-//    int count = 0;
-//    string exemplaireName = string.Empty;
-//    string exemplaireNameWithPref = string.Empty;
-//    string wallName = string.Empty;
-//    string floorName = string.Empty;
-//    string roofName = string.Empty;
-//    string ceilingName = string.Empty;
-//    double exemplaireWidth = 0;
-//    double totalExemplaireWidth = 0;
-
-//    foreach (var bultInCat in new BuiltInCategory[] { BuiltInCategory.OST_Walls, BuiltInCategory.OST_Floors, BuiltInCategory.OST_Roofs })
-//    {
-//        foreach (Element elType in new ElemensCollectionOfType().GetCollection(doc, bultInCat))
-//        {
-
-//            if (elType is WallType wallType)
-//            {
-//                try
-//                {
-//                    wallName = string.Empty;
-//                    exemplaireName = string.Empty;
-//                    count = 0;
-//                    totalExemplaireWidth = 0;
-
-//                    wallType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0);
-//                    wallType.Name = GetLayerForm(count, exemplaireName) + "_" + totalExemplaireWidth.ToString();
-//                }
-//                catch
-//                {
-//                }
-//            }
-
-//            if (elType is FloorType floorType)
-//            {
-//                try
-//                {
-//                    floorName = string.Empty;
-//                    count = 0;
-//                    totalExemplaireWidth = 0;
-//                    floorType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0);
-
-//                    floorType.Name = GetLayerForm(count, exemplaireName) + "_" + totalExemplaireWidth.ToString();
-//                }
-//                catch
-//                {
-//                }
-//            }
-
-//            if (elType is RoofType roofType)
-//            {
-//                try
-//                {
-//                    roofName = string.Empty;
-//                    count = 0;
-//                    totalExemplaireWidth = 0;
-//                    roofType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0);
-
-//                    roofType.Name = GetLayerForm(count, exemplaireName) + "_" + totalExemplaireWidth.ToString();
-
-
-//                }
-//                catch
-//                {
-//                }
-//            }
-
-//            if (elType is CeilingType ceilingType)
-//            {
-//                try
-//                {
-//                    ceilingName = string.Empty;
-//                    count = 0;
-//                    totalExemplaireWidth = 0;
-//                    foreach (CompoundStructureLayer layer in ceilingType.GetCompoundStructure().GetLayers().Where(v => v.Width != 0))
-//                    {
-
-//                        ElementId elId = layer.MaterialId;
-//                        Material exemplaireLayerMaterial = doc.GetElement(elId) as Material;
-//                        exemplaireName = ceilingName + exemplaireLayerMaterial.Name + '-' + Math.Round(layer.Width * 304.8).ToString();
-//                        count++;
-//                        totalExemplaireWidth = totalExemplaireWidth + Math.Round(layer.Width * 304.8);
-
-//                    }
-//                    ceilingType.Name = GetLayerForm(count, exemplaireName) + "_" + totalExemplaireWidth.ToString();
-//                }
-//                catch
-//                {
-//                }
-
-//            }
-
-
-
-//            //wallName = string.Empty;
-//            //count = 0;
-//            //totalExemplaireWidth = 0;
-
 
 
 
